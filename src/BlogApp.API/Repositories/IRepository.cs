e12204fa6061b4bce440944cab4dc158a329785d
Baris Ceviz <me@barisceviz.com>
2018-11-13 02:48:31 +0300

    Created repostiory base

---
+++ src/BlogApp.API/Repositories/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace BlogApp.API.Repositories
+{
+    public interface IRepository : IDisposable
+    {
+        Task<bool> SaveAsync();
+    }
+}
