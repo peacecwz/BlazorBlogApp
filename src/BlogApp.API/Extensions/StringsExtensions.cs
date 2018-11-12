031d28913bf7e3201f24868a9a392ea78a088058
Baris Ceviz <me@barisceviz.com>
2018-11-13 02:49:11 +0300

    Created strings extensions

---
+++ src/BlogApp.API/Extensions/StringsExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace BlogApp.API.Extensions
+{
+    public static class StringsExtensions
+    {
+        public static string ToSlug(this string title)
+        {
+            //TODO (peacecwz): Create slug
+            return title.ToLower().Replace(" ", "-");
+        }
+    }
+}
