9ecbf794b377b530c4c450407ab744c7b9e24a60
Baris Ceviz <me@barisceviz.com>
2018-11-13 02:43:20 +0300

    Created entity base

---
+++ src/BlogApp.API/Data/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BlogApp.API.Data
+{
+    public class EntityBase<TPrimary> where TPrimary : struct
+    {
+        public TPrimary Id { get; set; } = default(TPrimary);
+        public bool IsActive { get; set; } = true;
+        public bool IsDeleted { get; set; } = false;
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+        public DateTime? LastModifiedOn { get; set; }
+    }
+}
