7e78bd8538b25c3de8d33625673e32aa1e84b931
Baris Ceviz <me@barisceviz.com>
2018-11-13 02:46:38 +0300

    Added update category contract models

---
+++ src/shared/BlogApp.Contract/Request/Categories/UpdateCategoryRequest.cs
@@ -0,0 +1,10 @@
+﻿using BlogApp.Contract.Models;
+
+namespace BlogApp.Contract.Request.Categories
+{
+    public class UpdateCategoryRequest : RequestBase
+    {
+        public int CategoryId { get; set; }
+        public CategoryModel Category { get; set; }
+    }
+}
---
+++ src/shared/BlogApp.Contract/Response/Categories/UpdateCategoryResponse.cs
@@ -0,0 +1,9 @@
+﻿using BlogApp.Contract.Models;
+
+namespace BlogApp.Contract.Response.Categories
+{
+    public class UpdateCategoryResponse : ResponseBase
+    {
+        public CategoryModel Category { get; set; }
+    }
+}
