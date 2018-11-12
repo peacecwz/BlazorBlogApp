c15b2ad50abd5e8afc6c26a3926dcf45de2d0489
Baris Ceviz <me@barisceviz.com>
2018-11-13 02:45:08 +0300

    Added Create post contract models

---
+++ src/shared/BlogApp.Contract/Request/Posts/CreatePostRequest.cs
@@ -0,0 +1,9 @@
+﻿using BlogApp.Contract.Models;
+
+namespace BlogApp.Contract.Request.Posts
+{
+    public class CreatePostRequest : RequestBase
+    {
+        public PostModel Post { get; set; }
+    }
+}
---
+++ src/shared/BlogApp.Contract/Response/Posts/CreatePostResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace BlogApp.Contract.Response.Posts
+{
+    public class CreatePostResponse : ResponseBase
+    {
+    }
+}
