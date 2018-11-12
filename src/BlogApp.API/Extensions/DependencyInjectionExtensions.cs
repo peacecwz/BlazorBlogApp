3f2b83396e17ce77556dc643d8c333b4d6d86f8c
Baris Ceviz <me@barisceviz.com>
2018-11-13 02:49:19 +0300

    Created DI Extensions

---
+++ src/BlogApp.API/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using BlogApp.API.Mappers;
+using BlogApp.API.Mappers.Impl;
+using BlogApp.API.Repositories;
+using BlogApp.API.Repositories.Impl;
+using BlogApp.API.Services;
+using BlogApp.API.Services.Impl;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlogApp.API.Extensions
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static IServiceCollection AddMappersLayer(this IServiceCollection services)
+        {
+            services.AddTransient<IPostMapper, PostMapper>();
+            services.AddTransient<ICategoryMapper, CategoryMapper>();
+            return services;
+        }
+
+        public static IServiceCollection AddRepositoriesLayer(this IServiceCollection services)
+        {
+            services.AddScoped<IPostRepository, IPostRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            return services;
+        }
+
+        public static IServiceCollection AddServicesLayer(this IServiceCollection services)
+        {
+            services.AddScoped<IPostsService, PostsService>();
+            services.AddScoped<ICategoriesService, CategoriesService>();
+            return services;
+        }
+    }
+}
