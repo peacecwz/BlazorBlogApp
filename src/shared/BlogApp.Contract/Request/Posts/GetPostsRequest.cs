974b4fd5b69f4b357b3092498dab78171a5a841c
Baris Ceviz <me@barisceviz.com>
2018-11-13 02:45:40 +0300

    Added get posts contract models

---
+++ src/shared/BlogApp.Contract/Request/Posts/GetPostsRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace BlogApp.Contract.Request.Posts
+{
+    public class GetPostsRequest : RequestBase
+    {
+        public int PageIndex { get; set; } = 0;
+        public int ItemCountPerPage { get; set; } = 20;
+    }
+}
---
+++ src/shared/BlogApp.Contract/Response/Posts/GetPostsResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using BlogApp.Contract.Models;
+
+namespace BlogApp.Contract.Response.Posts
+{
+    public class GetPostsResponse : ResponseBase
+    {
+        public List<PostModel> Posts { get; set; }
+    }
+}
