78b235045a3fcdc4b49ec0a1b7d0af378ae1c71f
Baris Ceviz <me@barisceviz.com>
2018-11-13 02:49:28 +0300

    Created controller base

---
+++ src/BlogApp.API/Common/ApiControllerBase.cs
@@ -0,0 +1,16 @@
+﻿using BlogApp.Contract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.API.Common
+{
+    [ApiController]
+    public class ApiControllerBase : ControllerBase 
+    {
+        [NonAction]
+        public IActionResult GenerateResponse(ResponseBase response)
+        {
+            //TODO (peacecwz): Generate Response
+            return Ok(response);
+        }
+    }
+}
