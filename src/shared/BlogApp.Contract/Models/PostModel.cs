e735be072fe72840d27aced4dbb7c76e1563f840
Baris Ceviz <me@barisceviz.com>
2018-11-13 02:44:05 +0300

    Created post model

---
+++ src/shared/BlogApp.Contract/Models/PostModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BlogApp.Contract.Enums;
+
+namespace BlogApp.Contract.Models
+{
+    public class PostModel
+    {
+        public string Title { get; set; }
+        public Guid PostId { get; set; }
+        public string Slug { get; set; }
+        public string Description { get; set; }
+        public string Content { get; set; }
+        public PostContentType ContentType { get; set; }
+        public int CategoryId { get; set; }
+        public string Tags { get; set; }
+        public string CoverIamgeUrl { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime? PublishedDate { get; set; }
+        public Guid AuthorId { get; set; }
+    }
+}
