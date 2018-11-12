78d24d38836fd8d647320283a9f931a67be0cc9a
Baris Ceviz <me@barisceviz.com>
2018-11-13 02:45:22 +0300

    Added update post contract models

---
+++ src/shared/BlogApp.Contract/Request/Posts/UpdatePostRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using BlogApp.Contract.Models;
+
+namespace BlogApp.Contract.Request.Posts
+{
+    public class UpdatePostRequest : RequestBase
+    {
+        public Guid PostId { get; set; }
+        public PostModel Post { get; set; }
+    }
+}
---
+++ src/shared/BlogApp.Contract/Response/Posts/UpdatePostResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace BlogApp.Contract.Response.Posts
+{
+    public class UpdatePostResponse : ResponseBase
+    {
+    }
+}
