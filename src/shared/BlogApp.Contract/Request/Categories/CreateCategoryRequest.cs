a60572151133071002cbc813f1a733ca1e04faab
Baris Ceviz <me@barisceviz.com>
2018-11-13 02:46:26 +0300

    Added create category contract models

---
+++ src/shared/BlogApp.Contract/Request/Categories/CreateCategoryRequest.cs
@@ -0,0 +1,9 @@
+﻿using BlogApp.Contract.Models;
+
+namespace BlogApp.Contract.Request.Categories
+{
+    public class CreateCategoryRequest : RequestBase
+    {
+        public CategoryModel Category { get; set; }
+    }
+}
---
+++ src/shared/BlogApp.Contract/Response/Categories/CreateCategoryResponse.cs
@@ -0,0 +1,9 @@
+﻿using BlogApp.Contract.Models;
+
+namespace BlogApp.Contract.Response.Categories
+{
+    public class CreateCategoryResponse : ResponseBase
+    {
+        public CategoryModel Category { get; set; }
+    }
+}
