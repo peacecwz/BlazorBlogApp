14223155f9c28d36e1a3980adc345d1f4b31d47a
Baris Ceviz <me@barisceviz.com>
2018-11-13 02:48:01 +0300

    Implemented Category Mapper

---
+++ src/BlogApp.API/Mappers/Impl/CategoryMapper.cs
@@ -0,0 +1,26 @@
+﻿using BlogApp.API.Data.Entities;
+using BlogApp.Contract.Models;
+
+namespace BlogApp.API.Mappers.Impl
+{
+    public class CategoryMapper : ICategoryMapper
+    {
+        public CategoryEntity ToEntity(CategoryModel model)
+        {
+            return new CategoryEntity()
+            {
+                Id = model.Id,
+                Name = model.Name
+            };
+        }
+
+        public CategoryModel ToModel(CategoryEntity entity)
+        {
+            return new CategoryModel()
+            {
+                Id = entity.Id,
+                Name = entity.Name
+            };
+        }
+    }
+}
