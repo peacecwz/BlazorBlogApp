4ecc5169e2ff76aa96b2a671685322e12844d0e1
Baris Ceviz <me@barisceviz.com>
2018-11-13 02:48:54 +0300

    Implemented post repository layer

---
+++ src/BlogApp.API/Repositories/IPostRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BlogApp.API.Data.Entities;
+
+namespace BlogApp.API.Repositories
+{
+    public interface IPostRepository : IRepository
+    {
+        Task<IEnumerable<PostEntity>> GetPostsAsync(int pageIndex, int itemCountPerPage);
+        Task<PostEntity> GetPostByIdAsync(Guid postId);
+        Task<PostEntity> GetPostBySlugAsync(string slug);
+        Task<bool> IsExistsPostByTitleAsync(string title);
+        Task<bool> CreatePostAsync(PostEntity entity);
+        Task<bool> UpdatePostAsync(PostEntity entity);
+        Task<bool> DeletePostAsync(PostEntity entity);
+    }
+}
---
+++ src/BlogApp.API/Repositories/Impl/PostRepository.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogApp.API.Data;
+using BlogApp.API.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace BlogApp.API.Repositories.Impl
+{
+    public class PostRepository : IPostRepository
+    {
+        private readonly BlogAppDbContext _dbContext;
+        private readonly ILogger<PostRepository> _logger;
+        public PostRepository(BlogAppDbContext dbContext, ILogger<PostRepository> logger)
+        {
+            _dbContext = dbContext;
+            _logger = logger;
+        }
+
+        public void Dispose()
+        {
+            _dbContext?.Dispose();
+        }
+
+        public async Task<bool> SaveAsync()
+        {
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("An error occurred", ex);
+                return false;
+            }
+        }
+
+        public async Task<IEnumerable<PostEntity>> GetPostsAsync(int pageIndex, int itemCountPerPage)
+        {
+            return await _dbContext.Posts
+                .OrderByDescending(x => x.CreatedOn)
+                .Skip(pageIndex * itemCountPerPage)
+                .Take(itemCountPerPage)
+                .ToListAsync();
+        }
+
+        public async Task<PostEntity> GetPostByIdAsync(Guid postId)
+        {
+            return await _dbContext.Posts.FindAsync(postId);
+        }
+
+        public async Task<PostEntity> GetPostBySlugAsync(string slug)
+        {
+            return await _dbContext.Posts.SingleOrDefaultAsync(x => x.Slug == slug);
+        }
+
+        public async Task<bool> IsExistsPostByTitleAsync(string title)
+        {
+            return await _dbContext.Posts.AnyAsync(x => x.Title == title);
+        }
+
+        public async Task<bool> CreatePostAsync(PostEntity entity)
+        {
+            await _dbContext.Posts.AddAsync(entity);
+
+            return await SaveAsync();
+        }
+
+        public async Task<bool> UpdatePostAsync(PostEntity entity)
+        {
+            _dbContext.Posts.Update(entity);
+
+            return await SaveAsync();
+        }
+
+        public async Task<bool> DeletePostAsync(PostEntity entity)
+        {
+            _dbContext.Posts.Remove(entity);
+
+            return await SaveAsync();
+        }
+    }
+}
