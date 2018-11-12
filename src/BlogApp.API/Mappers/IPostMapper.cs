18055c4e1481af8bb7c752a21ba019b68e5711c0
Baris Ceviz <me@barisceviz.com>
2018-11-13 02:48:14 +0300

    Implemented Post Mapper

---
+++ src/BlogApp.API/Mappers/IPostMapper.cs
@@ -0,0 +1,9 @@
+﻿using BlogApp.API.Data.Entities;
+using BlogApp.Contract.Models;
+
+namespace BlogApp.API.Mappers
+{
+    public interface IPostMapper : IMapper<PostEntity, PostModel>
+    {
+    }
+}
---
+++ src/BlogApp.API/Mappers/Impl/PostMapper.cs
@@ -0,0 +1,46 @@
+﻿using BlogApp.API.Data.Entities;
+using BlogApp.Contract.Models;
+
+namespace BlogApp.API.Mappers.Impl
+{
+    public class PostMapper : IPostMapper
+    {
+        public PostEntity ToEntity(PostModel model)
+        {
+            return new PostEntity()
+            {
+                Id =  model.PostId,
+                Title = model.Title,
+                Slug = model.Slug,
+                Description = model.Description,
+                Content = model.Content,
+                ContentType = model.ContentType,
+                CategoryId = model.CategoryId,
+                Tags = model.Tags,
+                CoverImageUrl = model.CoverIamgeUrl,
+                CreatedOn = model.CreatedOn,
+                PublishedDate = model.PublishedDate,
+                AuthorId = model.AuthorId
+            };
+        }
+
+        public PostModel ToModel(PostEntity entity)
+        {
+            return new PostModel()
+            {
+                Slug = entity.Slug,
+                Title = entity.Title,
+                PostId = entity.Id,
+                CategoryId = entity.CategoryId,
+                CreatedOn = entity.CreatedOn,
+                PublishedDate = entity.PublishedDate,
+                AuthorId = entity.AuthorId,
+                Content = entity.Content,
+                ContentType = entity.ContentType,
+                CoverIamgeUrl = entity.CoverImageUrl,
+                Description = entity.Description,
+                Tags = entity.Tags
+            };
+        }
+    }
+}
