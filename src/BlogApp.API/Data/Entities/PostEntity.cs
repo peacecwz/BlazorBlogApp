62b313ccfe0740a6d782beb0d52f69d31c5eb55c
Baris Ceviz <me@barisceviz.com>
2018-11-13 02:43:38 +0300

    Created post enttity

---
+++ src/BlogApp.API/Data/Entities/PostEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BlogApp.Contract.Enums;
+
+namespace BlogApp.API.Data.Entities
+{
+    public class PostEntity : EntityBase<Guid>
+    {
+        public string Slug { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Content { get; set; }
+        public PostContentType ContentType { get; set; }
+        public int CategoryId { get; set; }
+        public string Tags { get; set; }
+        public string CoverImageUrl { get; set; }
+        public DateTime? PublishedDate { get; set; }
+        public Guid AuthorId { get; set; }
+    }
+}
