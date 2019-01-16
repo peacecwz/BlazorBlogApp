1d6769f583d9b72fea48ebf036f6d9e7e219ba23
Baris Ceviz <me@barisceviz.com>
2019-01-16 03:14:16 +0300

    Added Slug to category

--- src/BlogApp.API/Data/Entities/CategoryEntity.cs
+++ src/BlogApp.API/Data/Entities/CategoryEntity.cs
@@ -3,6 +3,7 @@
     public class CategoryEntity : EntityBase<int>
     {
         public string Name { get; set; }
+        public string Slug { get; set; }
         public bool IsSpotlight { get; set; }
     }
 }
